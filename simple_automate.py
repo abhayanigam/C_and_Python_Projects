b309ca3fccdf953257df634c639a451f0ff6bf27
Abhaya <abhaya.nigam13@gmail.com>
2020-10-06 22:30:41 +0530

    Simple auto mate messenger using python

---
+++ simple_automate.py
@@ -0,0 +1,9 @@
+"""
+first install module pip install pyautogui
+"""
+import pyautogui
+import time
+while True:
+    time.sleep(10)
+    pyautogui.typewrite('aurr batao.\nkya kr rhe hoo\nall good.')
+    pyautogui.press('enter')
