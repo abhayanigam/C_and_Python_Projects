0164923366451fe98b77d5f48902545277d7c227
Abhaya Nigam <61977981+abhayanigam@users.noreply.github.com>
2021-04-12 17:57:38 +0530

    Don't need this file

--- simple_automate.py
+++
@@ -1,9 +0,0 @@
-"""
-first install module pip install pyautogui
-"""
-import pyautogui
-import time
-while True:
-    time.sleep(10)
-    pyautogui.typewrite('aurr batao.\nkya kr rhe hoo\nall good.')
-    pyautogui.press('enter')
