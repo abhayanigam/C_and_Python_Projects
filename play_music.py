44d7a02167cba100432b5df37016f8f05ffe70cf
Abhaya Nigam <61977981+abhayanigam@users.noreply.github.com>
2021-04-12 17:57:27 +0530

    Don't need this file

--- play_music.py
+++
@@ -1,6 +0,0 @@
-"""
-install module pip install playsound
-and place the play.mp3 file in the same folder where your program is save.
-"""
-from playsound import playsound
-playsound('/Users/abhayanigam/Desktop/study/pythonProject/play.mp3')
